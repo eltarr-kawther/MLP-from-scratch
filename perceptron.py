7b13217a1e96e0cdf6d7cb6b1494c15e7e227f8c
eltarr-kawther <kawther.eltarr@laplateforme.io>
2022-09-21 22:29:37 +0200

    understood and renamed neuron

--- perceptron.py
+++ perceptron.py
@@ -1,27 +1,33 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 
-
-class Perceptron:
-    def __init__(self) -> None:
-        self.weights = 0
-        self.biais = 0
-        self.learningRate = 0
+class Neuron:
+    def __init__(self, weights, bias, learningRate) -> None:
+        self.weights = weights
+        self.bias = bias
+        self.learningRate = learningRate
     
-    def activate(self, signal):
+    def Activation(self, signal):
+        """
+        This is the Soma method
+        """
         if signal > 0:
             return 1
         else:
             return 0
     
-    def propagation(self, inputs):
-        propagate_signal = np.dot(self.weights, inputs) + self.bias
-        return self.activate(propagate_signal)
+    def Propagation(self, inputs):
+        """
+        This is the Axon method
+        """
+        propagated_signal = np.dot(self.weights, inputs) + self.bias
+        return self.Activation(propagated_signal)
     
-    def train(self, inputs, output):
-        output_prev = self.propagation(inputs)
-        self.weights = [W + X * self.learningRate * (output - output_prev) for (W, X) in zip(self.weights, inputs)]
-        self.bias = self.bias + self.learningRate * (output - output_prev)
-        error = np.abs(output_prev - output)
+    def Learning(self, inputs, output):
+        """
+        This is how a neuron learns
+        """
+        prev_output = self.Propagation(inputs)
+        self.weights = [W + X*self.learningRate*(output - prev_output) for (W, X) in zip(self.weights, inputs)]
+        self.bias = self.bias + self.learningRate*(output - prev_output)
+        error = np.abs(prev_output - output)
         return error
