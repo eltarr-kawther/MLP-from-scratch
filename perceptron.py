80a7d54b84a963162d64e60115e7cb977d254ed9
eltarr-kawther <kawther.eltarr@laplateforme.io>
2022-10-13 12:16:14 +0200

    fixed dummy data viz

--- perceptron.py
+++ perceptron.py
@@ -10,10 +10,7 @@ class Neuron:
         """
         This is the Soma method
         """
-        if signal > 0:
-            return 1
-        else:
-            return 0
+        return 1 if signal > 0 else 0
     
     def Propagation(self, inputs):
         """
@@ -27,7 +24,7 @@ class Neuron:
         This is how a neuron learns
         """
         prev_output = self.Propagation(inputs)
-        self.weights = [W + X*self.learningRate*(output - prev_output) for (W, X) in zip(self.weights, inputs)]
+        self.weights = [W + X * self.learningRate * (output - output_prev) for (W, X) in zip(self.weights, inputs)]
         self.bias = self.bias + self.learningRate*(output - prev_output)
         error = np.abs(prev_output - output)
         return error
