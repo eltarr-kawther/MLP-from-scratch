787ba3f7eb7b8839138a13a2792e6366f1fb0a3d
eltarr-kawther <kawther.eltarr@laplateforme.io>
2022-10-14 10:15:00 +0200

    fixed equation

--- perceptron.py
+++ perceptron.py
@@ -6,25 +6,26 @@ class Perceptron:
         self.bias = bias
         self.learningRate = learningRate
     
-    def Activation(self, signal):
+    def Activation(self, net):
         """
         This is the Soma method
         """
-        return np.where(signal>0, 1, 0)
+        return np.where(net>0, 1, 0)
     
     def Propagation(self, inputs):
         """
         This is the Axon method
         """
-        propagated_signal = np.dot(inputs, self.weights) + self.bias
-        return self.Activation(propagated_signal)
+        net = np.dot(self.weights, inputs) + self.bias
+        return self.Activation(net)
     
     def Learning(self, inputs, output):
         """
         This is how a Neuron learns
         """
-        prev_output = self.Propagation(inputs)
-        self.weights = [W + X * self.learningRate * (output - output_prev) for (W, X) in zip(self.weights, inputs)]
-        self.bias = self.bias + self.learningRate*(output - prev_output)
-        error = np.abs(prev_output - output)
+        initial_output = self.Propagation(inputs)
+        self.weights = self.weights + inputs * self.learningRate * (output - initial_output)
+        #self.weights = [W + X * self.learningRate * (output - initial_output) for (W, X) in zip(self.weights, inputs)]
+        self.bias = self.bias + self.learningRate*(output - initial_output)
+        error = np.abs(initial_output - output)
         return error
